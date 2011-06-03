486774e8491d2f7a0de9e3161a27361ad1d7087e
Diogo Kamioka <farofitus@gmail.com>
2011-06-03 01:11:37 -0300

    Lets Rock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmporiumsjSales::Application.config.secret_token = '9a9c03771bfd23df3fb0ce66e3407fdbae4452411a7610f2a60653689eb8d7d7b6ff029d26f7dc1c514901a5f7946581b385a2cadf1cf02afa288a475824a8cf'
